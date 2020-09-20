8f7b5a177b14528bbb7a7e441a123dc179661ba4
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-09-20 15:59:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -53,7 +53,9 @@ class Dog
     dog = DB[:conn].execute(sql, name, breed).first
     
     if dog 
-      new_dog = self.new_from_db
+      new_dog = self.new_from_db(dog)
+    else 
+      
   end
   
 end
